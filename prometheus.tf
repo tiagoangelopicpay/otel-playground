615e43fd22ade500944b2482dbb940b1c4719796
Tiago Angelo <tiago.angelo@picpay.com>
2022-10-19 18:04:54 -0300

    Install Prometheus

---
+++ prometheus.tf
@@ -0,0 +1,25 @@
+resource "helm_release" "prometheus" {
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  name       = "prometheus"
+  version    = "15.16.1"
+  namespace  = kubernetes_namespace_v1.prometheus.metadata[0].name
+
+  values = [
+    yamlencode({
+      nodeExporter     = { enabled = false }
+      kubeStateMetrics = { enabled = false }
+      pushgateway      = { enabled = false }
+      alertmanager     = { enabled = false }
+      serverFiles      = { "prometheus.yml" = { scrape_configs = [] } }
+      server           = {
+        extraFlags = ["web.enable-remote-write-receiver"]
+        ingress    = { enabled = true, hosts = ["prometheus.lvh.me"], ingressClassName = "nginx" }
+      }
+    })
+  ]
+}
+
+resource "kubernetes_namespace_v1" "prometheus" {
+  metadata { name = "prometheus" }
+}
